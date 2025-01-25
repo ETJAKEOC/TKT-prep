d729eb66bf1093df9d384aa17b1a5220dec801e8
ETJAKEOC <etjakeoc@gmail.com>
2025-01-25 00:38:30 -0500

    updates

--- install.sh
+++ install.sh
@@ -123,7 +123,7 @@ _prompt_user() {
         if [ -f "patches/more-uarches.patch" ]; then
             echo "Applying more-uarches.patch..."
 	    cd $_BUILD_DIR
-            patch -Np1 < $_SCRIPT_DIR/more-uarches.patch
+            patch -Np1 < $_SCRIPT_DIR/patches/more-uarches.patch
         else
             echo "more-uarches.patch not found."
             exit 1
