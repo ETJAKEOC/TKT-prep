70224a354af2e701d8c1093d831e84e8ca464a01
ETJAKEOC <etjakeoc@gmail.com>
2025-01-26 16:23:33 -0500

    Added fun new error message :)

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -4,10 +4,12 @@
 compile_kernel() {
     echo "Compiling the kernel..."
     if [[ "$_COMPILER" == "clang" ]]; then
-	    time make -j$_MAKE_JOBS $_LLVM_ENV CFLAGS="$CFLAGS $_CFLAGS $_MAKE_O" bzImage modules headers
+        time make -j$_MAKE_JOBS $_LLVM_ENV CFLAGS="$CFLAGS $_CFLAGS $_MAKE_O" bzImage modules headers
         elif [[ "$_COMPILER" == "gcc" ]]; then
-	    time make -j$_MAKE_JOBS $_GCC_ENV CFLAGS="$CFLAGS $_CFLAGS $_MAKE_O" bzImage modules headers
-	else
-	    [ $? -ne 0 ] && { echo "Kernel compilation failed."; }
+        time make -j$_MAKE_JOBS $_GCC_ENV CFLAGS="$CFLAGS $_CFLAGS $_MAKE_O" bzImage modules headers
+    else
+            echo "WTF are you trying to compile my precious kernel with, are you drunk or smth???"
+            exit 1
     fi
+    [ $? -ne 0 ] && { echo "Kernel compilation failed."; exit 1; }
 }
