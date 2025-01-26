98fd5d9d55dde91fca88dd819ab1de8a10b3d99b
ETJAKEOC <etjakeoc@gmail.com>
2025-01-26 11:52:52 -0500

    Trying to get CPU march applied to kernel config

--- install.sh
+++ install.sh
@@ -101,6 +101,103 @@ _configure_kernel() {
             ;;
     esac
 
+    # Set the CPU architecture in the kernel configuration
+    if [[ "$_CPU_OPTIMIZE" =~ ^(yes|y)$ ]]; then
+        case $_CPU_MARCH in
+            "znver1")
+                CONFIG_CPU="CONFIG_MZEN"
+                ;;
+            "znver2")
+                CONFIG_CPU="CONFIG_MZEN2"
+                ;;
+            "znver3")
+                CONFIG_CPU="CONFIG_MZEN3"
+                ;;
+            "znver4")
+                CONFIG_CPU="CONFIG_MZEN4"
+                ;;
+            "znver5")
+                CONFIG_CPU="CONFIG_MZEN5"
+                ;;
+            "nehalem")
+                CONFIG_CPU="CONFIG_MNEHALEM"
+                ;;
+            "westmere")
+                CONFIG_CPU="CONFIG_MWESTMERE"
+                ;;
+            "silvermont")
+                CONFIG_CPU="CONFIG_MSILVERMONT"
+                ;;
+            "goldmont")
+                CONFIG_CPU="CONFIG_MGOLDMONT"
+                ;;
+            "goldmont-plus")
+                CONFIG_CPU="CONFIG_MGOLDMONTPLUS"
+                ;;
+            "sandybridge")
+                CONFIG_CPU="CONFIG_MSANDYBRIDGE"
+                ;;
+            "ivybridge")
+                CONFIG_CPU="CONFIG_MIVYBRIDGE"
+                ;;
+            "haswell")
+                CONFIG_CPU="CONFIG_MHASWELL"
+                ;;
+            "broadwell")
+                CONFIG_CPU="CONFIG_MBROADWELL"
+                ;;
+            "skylake")
+                CONFIG_CPU="CONFIG_MSKYLAKE"
+                ;;
+            "skylake-avx512")
+                CONFIG_CPU="CONFIG_MSKYLAKEX"
+                ;;
+            "cannonlake")
+                CONFIG_CPU="CONFIG_MCANNONLAKE"
+                ;;
+            "icelake-client")
+                CONFIG_CPU="CONFIG_MICELAKE"
+                ;;
+            "cascadelake")
+                CONFIG_CPU="CONFIG_MCASCADELAKE"
+                ;;
+            "cooperlake")
+                CONFIG_CPU="CONFIG_MCOOPERLAKE"
+                ;;
+            "tigerlake")
+                CONFIG_CPU="CONFIG_MTIGERLAKE"
+                ;;
+            "sapphirerapids")
+                CONFIG_CPU="CONFIG_MSAPPHIRERAPIDS"
+                ;;
+            "rocketlake")
+                CONFIG_CPU="CONFIG_MROCKETLAKE"
+                ;;
+            "alderlake")
+                CONFIG_CPU="CONFIG_MALDERLAKE"
+                ;;
+            "raptorlake")
+                CONFIG_CPU="CONFIG_MRAPTORLAKE"
+                ;;
+            "meteorlake")
+                CONFIG_CPU="CONFIG_MMETEORLAKE"
+                ;;
+            "emeraldrapids")
+                CONFIG_CPU="CONFIG_MEMERALDRAPIDS"
+                ;;
+            *)
+                CONFIG_CPU=""
+                ;;
+        esac
+
+        if [ -n "$CONFIG_CPU" ]; then
+            echo "Setting kernel CPU architecture to $_CPU_MARCH ($CONFIG_CPU)"
+            sed -i "s/# $CONFIG_CPU is not set/$CONFIG_CPU=y/g" .config
+        else
+            echo "CPU architecture $_CPU_MARCH not recognized, defaulting to generic-x86-64"
+        fi
+    fi
+
     [ "$_CONFIG_TOOL" != "skip" ] && make $_CONFIG_TOOL && echo "Kernel configuration completed." || echo "Skipping configuration step."
 }
 
@@ -202,7 +299,9 @@ _main() {
     if [[ "$_CPU_OPTIMIZE" =~ ^(yes|y)$ ]]; then
         if [ -z "$_CPU_MARCH" ]; then
             if [[ "$_COMPILER" == "clang" ]]; then
-                _CPU_MARCH=$(clang -march=native -### 2>&1 | grep -- '-target-cpu' | awk '{print $2}')
+#	For some reason, clang does not properly report CPU march all the time, so we use GCC as a hack for now.
+#                _CPU_MARCH=$(clang -march=native -### 2>&1 | grep -- '-target-cpu' | awk '{print $2}')
+                _CPU_MARCH=$(gcc -march=native -Q --help=target | grep -- '-march=' | awk '{print $2}')
                 CFLAGS="-pipe -march=$_CPU_MARCH -mtune=$_CPU_MARCH -flto"
             elif [[ "$_COMPILER" == "gcc" ]]; then
                 _CPU_MARCH=$(gcc -march=native -Q --help=target | grep -- '-march=' | awk '{print $2}')
@@ -212,6 +311,7 @@ _main() {
                 exit 1
             fi
         fi
+        echo "Detected CPU architecture: $_CPU_MARCH"
     else
         _CPU_MARCH="x86-64-v1"
     fi
