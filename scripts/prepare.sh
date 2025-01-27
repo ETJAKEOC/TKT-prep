7b027b911b6fb9c73f2a3f67b880d62d202d9e55
ETJAKEOC <etjakeoc@gmail.com>
2025-01-26 21:28:32 -0500

    Ensuring that the config file is properly copied to the kernel compile env

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -2,6 +2,7 @@
 # prepare.sh
 
 prepare_env_and_source() {
+#Check the current distribution
 	if [ -f /etc/os-release ]; then
 		. /etc/os-release
 		_DISTRO=$ID
@@ -10,6 +11,7 @@ prepare_env_and_source() {
 		exit 1
 	fi
 
+# If building kernel 6.12/6.13, we pull from the FreeDesktop DRM repo, to take advantage of GPU patches.
 	if [ "${_KERNEL_VERSION}" = "6.12" ] || [ "${_KERNEL_VERSION}" = "6.13" ]; then
 		KERNEL_URL="https://gitlab.freedesktop.org/agd5f/linux/-/archive/drm-fixes-${_KERNEL_VERSION}/linux-drm-fixes-${_KERNEL_VERSION}.tar.gz"
 		TAR_FILE="linux-${_KERNEL_VERSION}.tar.gz"
@@ -18,6 +20,7 @@ prepare_env_and_source() {
 		TAR_FILE="linux-${_KERNEL_VERSION}.tar.xz"
 	fi
 
+# Extract the sources, and if they were already extracted, cleanup
 	if [ -f "$TAR_FILE" ]; then
 		echo "Kernel tarball already downloaded. Not redownloading."
 		else
@@ -33,5 +36,18 @@ prepare_env_and_source() {
 	echo "Extracting kernel source to $_BUILD_DIR..."
 	mkdir -p $_BUILD_DIR
 	tar -xf $TAR_FILE -C $_BUILD_DIR --strip-components=1
+
+# Copy the user-specified config file to the build directory as .config
+	if [ -n "$_CONFIG_FILE" ]; then
+		if [ -f "$_CONFIG_FILE" ]; then
+		cp "$_CONFIG_FILE" "$_BUILD_DIR/.config"
+			echo "Copied custom config file to $_BUILD_DIR/.config"
+		else
+			echo "Specified config file $_CONFIG_FILE does not exist."
+			exit 1
+		fi
+	else
+		echo "No custom config file specified."
+	fi
 	cd $_BUILD_DIR
 }
